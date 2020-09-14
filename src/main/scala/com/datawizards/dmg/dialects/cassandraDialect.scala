b22d34e4b1ee50339a8c314090fb6eeb8ca95575
doofin <8177dph@gmail.com>
2020-09-14 22:27:27 +0800

    cassandraDialect

---
+++ src/main/scala/com/datawizards/dmg/dialects/cassandraDialect.scala
@@ -0,0 +1,110 @@
+package com.datawizards.dmg.dialects
+
+import com.datawizards.dmg.metadata._
+/** [[io.getquill.context.cassandra.encoding ]]*/
+object cassandraDialect extends DatabaseDialect {
+  override def intType: String = "int"
+
+  override def stringType: String = "text"
+
+  override def longType: String = "bigint"
+
+  override def binaryType: String = "blob"
+
+  override def doubleType: String = "DOUBLE"
+
+  override def floatType: String = "REAL"
+
+  override def shortType: String = "SMALLINT"
+
+  override def booleanType: String = "BOOLEAN"
+
+  override def byteType: String = "TINYINT"
+
+  override def dateType: String = "DATE"
+
+  override def timestampType: String = "TIMESTAMP"
+
+
+
+  override def bigDecimalType: String = "DECIMAL(38,18)"
+
+  override def bigIntegerType: String = "BIGINT"
+
+  override def generateArrayTypeExpression(elementTypeExpression: String): String = "ARRAY"
+
+  override def generateClassTypeExpression(classTypeMetaData: ClassTypeMetaData, fieldNamesWithExpressions: Iterable[(String, String)]): String = "OTHER"
+
+  override def generateMapTypeExpression(keyExpression: String, valueExpression: String): String = {
+    log.warn("H2 doesn't support Map type. Column converted to VARCHAR")
+    "VARCHAR"
+  }
+
+  override def toString: String = "H2Dialect"
+
+  override protected def fieldAdditionalExpressions(f: ClassFieldMetaData): String =
+    notNullExpression(f) +
+    commentExpression(f)
+
+  override protected def additionalTableProperties(classTypeMetaData: ClassTypeMetaData): String = ""
+
+  override protected def additionalTableExpressions(classTypeMetaData: ClassTypeMetaData): String =
+    if(comment(classTypeMetaData).isDefined)
+      s"""
+         |COMMENT ON TABLE ${classTypeMetaData.typeName} IS '${comment(classTypeMetaData).get}';""".stripMargin
+    else ""
+
+  private def notNullExpression(f: ClassFieldMetaData): String =
+    if(notNull(f)) " NOT NULL" else ""
+
+  private def commentExpression(f: ClassFieldMetaData): String =
+    if(comment(f).isEmpty) "" else s" COMMENT '${comment(f).get}'"
+
+  override protected def reservedKeywords = Seq(
+    "ALL",
+    "ALTER",
+    "AND",
+    "AS",
+    "BETWEEN",
+    "CASE",
+    "COLUMN",
+    "CREATE",
+    "DATABASE",
+    "DATE",
+    "DELETE",
+    "DISTINCT",
+    "DROP",
+    "ELSE",
+    "END",
+    "EXISTS",
+    "FALSE",
+    "FETCH",
+    "FULL",
+    "GRANT",
+    "GROUP",
+    "HAVING",
+    "INNER",
+    "INSERT",
+    "INTO",
+    "JOIN",
+    "LEFT",
+    "NOT",
+    "NULL",
+    "OR",
+    "ORDER",
+    "OUTER",
+    "SELECT",
+    "TABLE",
+    "TRUE",
+    "UNION",
+    "UPDATE",
+    "USER",
+    "USING",
+    "VALUES",
+    "WHEN",
+    "WHERE"
+  )
+
+  override protected def escapeColumnName(columnName: String) =
+    s""""$columnName""""
+}
